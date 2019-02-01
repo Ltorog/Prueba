2910e6609122b523ce2ddd8a12cccf46cd46a207
Francisco Javier Sánchez Fuentes <fransafu@gmail.com>
2019-01-31 22:30:17 -0300

    Simplificar servidor (server.js)

--- Api/models/index.js
+++ Api/models/index.js
@@ -5,7 +5,12 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const config = require(__dirname + '/../config/config.json')[env];
+const config = {
+  database: 'database',
+  username: 'username',
+  password: 'password'
+}
 const db = {};
 
 let sequelize;
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
+const express = require('express');
+const http = require('http');
 const bodyParser = require('body-parser');
-const express= require('express');
+
+const PORT = process.env.PORT || '3000'; // Variable de entorno o puerto 3000
 
 const app = express();
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
-//Rutas
+// Rutas
 require('./Api/routes/students')(app);
 
 
-//Cabeceras
+// Cabeceras
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Methos');
@@ -19,11 +22,14 @@ app.use((req, res, next) => {
     next();
 });
 
-
-//Conexión
-app.get('*', (req, res) => {
+// Conexion
+app.get('/', (req, res) => {
     res.status(200).send({ message: "Bienvenido" });
 })
 
+const server = http.createServer(app);
 
-module.exports = app;
+server.listen(PORT, function() {
+  console.log('Servidor disponible en el puerto: ' + PORT);
+});
+
